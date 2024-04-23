cedbf1af3f440e20697c7d956102d79fd6473e0e
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-04-23 20:28:27 +0300

    add changews

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,3 +1,4 @@
+import pdfkit
 from aiogram import types
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, FSInputFile
@@ -7,6 +8,7 @@ from keyboards import reply
 from auth import main as auth
 import pandas as pd
 import requests
+import qrcode
 
 dp = main.dp
 
@@ -58,7 +60,6 @@ async def generate_exel(msg: types.Message):
         data["Category"].append(product["category"])
         data["Image"].append(product["image"])
 
-
     df = pd.DataFrame(data)
 
     writer = pd.ExcelWriter('files/expl.xlsx', engine='openpyxl')
@@ -69,3 +70,55 @@ async def generate_exel(msg: types.Message):
 
     await main.bot.send_document(chat_id=msg.from_user.id,
                                  document=FSInputFile("files/expl.xlsx"))
+
+
+@dp.message(Command("pdf"))
+async def generate_pdf(msg: types.Message):
+    html_content = """
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<style>
+h1{
+color: #f00;
+}
+</style>
+<body>
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+</body>
+</html>
+    """
+    path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+    output_file = 'files/tpl.pdf'
+    pdfkit.from_string(html_content, output_file, configuration=config)
+
+    await main.bot.send_document(chat_id=msg.from_user.id,
+                                 document=FSInputFile(output_file))
+
+
+@dp.message(Command("qr"))
+async def generate_qr(msg: types.Message):
+    # Create a QR code object with a larger size and higher error correction
+    qr = qrcode.QRCode(version=3, box_size=20, border=10, error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+    # Define the data to be encoded in the QR code
+    data = "https://www.youtube.com/watch?v=xgQSGD7ok7s"
+
+    # Add the data to the QR code object
+    qr.add_data(data)
+
+    # Make the QR code
+    qr.make(fit=True)
+
+    # Create an image from the QR code with a black fill color and white background
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save the QR code image
+    img.save("files/qr_code.png")
+
+    await main.bot.send_photo(chat_id=msg.from_user.id,
+                              photo=FSInputFile('files/qr_code.png'))
