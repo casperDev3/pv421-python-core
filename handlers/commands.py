f38f482ba36acd41f6b6a2591b704b6fda538f17
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2024-04-23 19:04:47 +0300

    generate exel

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,10 +1,12 @@
 from aiogram import types
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.utils.markdown import hbold
 from creds import main
 from keyboards import reply
 from auth import main as auth
+import pandas as pd
+import requests
 
 dp = main.dp
 
@@ -34,3 +36,36 @@ async def welcome_admin(msg: types.Message):
         await msg.answer("Вітає вам у режимі адміністратора", reply_markup=reply.admin_main())
     else:
         await msg.answer("You haven't access!", reply_markup=reply.main_menu())
+
+
+@dp.message(Command("exel"))
+async def generate_exel(msg: types.Message):
+    products = requests.get("https://fakestoreapi.com/products")
+    products = products.json()
+
+    data = {
+        'Title': [],
+        'Price': [],
+        'Description': [],
+        'Category': [],
+        'Image': []
+    }
+
+    for product in products:
+        data["Title"].append(product["title"])
+        data["Price"].append(product["price"])
+        data["Description"].append(product["description"])
+        data["Category"].append(product["category"])
+        data["Image"].append(product["image"])
+
+
+    df = pd.DataFrame(data)
+
+    writer = pd.ExcelWriter('files/expl.xlsx', engine='openpyxl')
+
+    df.to_excel(writer, sheet_name="Users", index=False)
+
+    writer._save()
+
+    await main.bot.send_document(chat_id=msg.from_user.id,
+                                 document=FSInputFile("files/expl.xlsx"))
